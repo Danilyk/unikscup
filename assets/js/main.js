c3343b83c81f3f08205d6724af6242bc06cd5b4e
Gigsterdev <gigsterdev18@gmail.com>
2016-03-20 19:36:21 +0700

    refactor css code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,31 +1,22 @@
 
 $(document).ready(function() {
-	$(".bicon_bicon_menu").on("click", function() {
-		var isOpened = $(".nav").hasClass('is-opened');
+	$('.form-step.step-1').on('click', ' .button-checkout', function (event) {
+		event.preventDefault();
 
-		if (isOpened) {
-			$(".nav").removeClass('is-opened');
-			$(".page").removeClass('menu-opened');
-			$(this).css('background', 'url(img/menu.png) center center no-repeat');
-		} else {
-			$(".nav").addClass('is-opened');
-			$(".page").addClass('menu-opened');
-			$(this).css('background', 'url(img/clear.png) center center no-repeat');
-		}
+		$('.form-step.step-1').removeClass('active');
+		$('.form-step.step-2').addClass('active');
+
+		$('.order-progress-bar .step-1').removeClass('active').addClass('done');
+		$('.order-progress-bar .step-2').addClass('active');
+	});
+
+	$('.form-step.step-2').on('click', ' .button-checkout', function (event) {
+		event.preventDefault();
+
+		$('.form-step.step-2').removeClass('active');
+		$('.form-step.step-3').addClass('active');
+
+		$('.order-progress-bar .step-2').removeClass('active').addClass('done');
+		$('.order-progress-bar .step-3').addClass('active');
 	});
 });
-
-function initMap() {
-  var myLatLng = {lat: 62.531392, lng: 113.977136};
-
-  var map = new google.maps.Map(document.getElementById('mapBox'), {
-    zoom: 17,
-    center: myLatLng
-  });
-
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
-}
